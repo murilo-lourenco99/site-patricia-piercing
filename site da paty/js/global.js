fef29256b370196a428317230c570a216ac30cf9
Murilo Lourenço de Jesus <muriloljesus22@gmail.com>
2026-01-28 17:29:25 -0300

    Delete site da paty directory

--- site da paty/js/global.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const footerP = document.querySelector(".main-footer p");
-    if(footerP) footerP.innerHTML = `&copy; ${new Date().getFullYear()} Patrícia Lourenço`;
-});
